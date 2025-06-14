36a39222f7f45abd5a9a337eeab2c009503b9469
ItsAltoo <itsmalik57@gmail.com>
2025-06-14 08:38:11 +0800

    Refactor dashboard URLs and views; update templates and styles
    - Changed the URL path for the 'rekap' view in dashboard to the root path.
    - Removed the rekap function from mysite views as it is now handled in the dashboard app.
    - Updated various HTML templates to improve layout and styling, including adjustments to margins and paddings.
    - Added new CSS classes for better styling and responsiveness.
    - Introduced animations for hero images and improved the navbar button functionality.
    - Added new binary files for compiled Python files in __pycache__ directories.

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
 
 from .views import rekap
 
 urlpatterns = [
-    path('rekap/', rekap, name='rekap' )
+    path('', rekap, name='rekap' )
 ]
 
